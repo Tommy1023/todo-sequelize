893bbd162d94682a0187f28d56940cf3999395b3
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-20 00:27:43 +0800

    feat: add create todo function & revice home router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,8 +5,12 @@ const db = require('../../models')
 const Todo = db.Todo
 
 router.get('/', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
+  const UserId = req.user.id
+  return Todo.findAll({
+    where: { UserId },
+    raw: true,
+    nest: true
+  })
     .then((todos) => { return res.render('index', { todos: todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
